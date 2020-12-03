dbe37608b457400ea51651572246ca45e9371719
euisuny <ey222@cornell.edu>
2020-12-03 01:36:53 -0500

    Polish

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,6 +1,5 @@
 module Day3 where
 
-import Control.Lens
 import Data.Char
 import Data.List
 
@@ -9,22 +8,14 @@ main = do
   input <- readFile "../input/day03"
   let parse = map (map digitToInt) . lines
   let geo = parse input
-  let a = solve geo (1, 1) (0, 0) 0
-  let b = solve geo (3, 1) (0, 0) 0
-  let c = solve geo (5, 1) (0, 0) 0
-  let d = solve geo (7, 1) (0, 0) 0
-  let e = solve geo (1, 2) (0, 0) 0
-  -- part1
-  print $ b
-  -- part2
-  print $ (a * b * c * d * e)
+  print $ part1 geo
+  print $ part2 geo
 
-nextIndex :: [[Int]] -> (Int, Int) -> (Int, Int) -> Maybe Int
-nextIndex geo (dx, dy) (i, j) = do
-  row <- geo ^? element (dy + j)
-  return $ row !! ((dx + i) `mod` (length $ geo !! 0))
+solve :: [[Int]] -> (Int, Int) -> Int
+solve geo (x, y) =
+  sum [cycle (geo !! y) !! x | (x, y) <- zip [0, x..] [0, y..length geo - 1]]
 
-solve geo (dx, dy) (i, j) acc =
-  case nextIndex geo (dx, dy) (i, j) of
-    Nothing -> acc
-    Just val -> solve geo (dx, dy) ((dx + i) `mod` (length $ geo !! 0), dy + j) (acc + val)
+solver l geo = product $ solve geo <$> l
+
+part1 = solver [(3, 1)]
+part2 = solver [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
